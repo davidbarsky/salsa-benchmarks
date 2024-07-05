ee83aa6d8038a8eaf40810d34807b2116e3e3aec
Micha Reiser <micha@reiser.io>
2024-07-05 15:40:10 +0200

    fix: Use single Salsa input (#1)

--- new-salsa/benches/compare.rs
+++ new-salsa/benches/compare.rs
@@ -4,6 +4,9 @@ use new_salsa::Database;
 fn compare(c: &mut Criterion) {
     let mut group = c.benchmark_group("compare");
     let mut db = Database::default();
+
+    let input = new_salsa::Input::new(&db, String::new());
+
     for n in &[1] {
         group.bench_function(BenchmarkId::new("new_salsa_constant", n), |b| {
             // let text = std::iter::repeat("A").take(*n).collect::<String>();
@@ -14,7 +17,7 @@ fn compare(c: &mut Criterion) {
     for n in &[1] {
         group.bench_function(BenchmarkId::new("new_salsa_length", n), |b| {
             let text = std::iter::repeat("A").take(*n).collect::<String>();
-            b.iter(|| new_salsa::run_length(&mut db, text.clone()))
+            b.iter(|| new_salsa::run_length(&mut db, input, text.clone()))
         });
     }
 
